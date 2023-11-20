b9e153abe94ccf80fa3d30fe066316fe791aa0b2
andreich1rkov <andrmen04@gmail.com>
2023-11-21 02:32:22 +0500

    Добавлены логи и лог файл

--- Bo4onki.py
+++ Bo4onki.py
@@ -2,17 +2,20 @@ import random
 import logging
 
 # Добавление Лог файла
-logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w")
+logging.basicConfig(level=logging.INFO, filename="Bo4onki_log.log",filemode="w")
 
 # Ввод и проверка на валидность числа N.
 while True:
     try:
         N = int(input("Введите целое положительное число N: "))
+        logging.info(f" Number of Bo4ki == {N}")
         if N < 1:
+            logging.error(" Error. Invalid 'N'", exc_info=True)
             raise ValueError
         break
-    except ValueError:
+    except ValueError as err:
          print("Ошибка")
+         logging.error(" Error. Invalid 'N'", exc_info=True)
 
 # Создание 2х списков: "Мешка" и "Стола". Заполнение Мешка бочонками.
 Bag = []
@@ -20,6 +23,8 @@ Table = []
 for i in range(1, N+1):
     Bag.append(i)
 print(Bag)
+logging.info(f" In the Bag: {Bag}")
+
 
 # Выбираем случайный бочонок из Мешка и кладем его на Cтол.
 while Bag != []:
@@ -27,17 +32,25 @@ while Bag != []:
         try:
             Button = int(input("Введите 1, чтобы вытащить один бочонок из мешка or Введите 2, чтобы завершить программу: "))
             if Button not in range(1, 3):
+                logging.error(" Error. Invalid 'N'", exc_info=True)
                 raise ValueError
             break
         except ValueError:
             print("Ошибка")
+            logging.error(" Error. Invalid 'N'", exc_info=True)
     if Button == 1:
+        logging.info(f" Button pressed: {Button}")
         x = random.choice(Bag)
         Bag.remove(x)
+        logging.info(f" Bo4ka #{x} removed from Bag")
         Table.append(x)
         print(f"{Bag} Мешок")
+        logging.info(f" {Bag} left in Bag")
         print(f"{Table} бочонки на столе")
+        logging.info(f" {Table} on Table")
     else:
+        logging.info(f" Button pressed: {Button}. Program ended.")
         exit()
 else:
     print("Мешочек пуст :(")
+    logging.info(" Bag is empty :(")
